a3f5c7893a0f9bdabc37f07532165b5e4056b3c9
Nikita Obergan <Obergannikita@gmail.com>
2023-07-26 00:26:44 -0400

    Add class ComputerPlayer, and implimented ChooseGesture method

---
+++ RPSLS/ComputerPlayer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    public class ComputerPlayer : Player
+    {
+        Random rnd = new Random();
+        int number;
+        public ComputerPlayer(string name) : base(name)
+        {
+        }
+        public override void ChooseGesture()
+        {
+            number = rnd.Next(0, 5);
+
+            if (number == 0)
+            {
+                chosenGesture = gestures[0];
+                Console.WriteLine($"{name} chooses {chosenGesture.name}");
+            }
+            else if (number == 1)
+            {
+                chosenGesture = gestures[1];
+                Console.WriteLine($"{name} chooses {chosenGesture.name}");
+            }
+            else if (number == 2)
+            {
+                chosenGesture = gestures[2];
+                Console.WriteLine($"{name} chooses {chosenGesture.name}");
+            }
+            else if (number == 3)
+            {
+                chosenGesture = gestures[3];
+                Console.WriteLine($"{name} chooses {chosenGesture.name}");
+            }
+            else if (number == 4)
+            {
+                chosenGesture = gestures[4];
+                Console.WriteLine($"{name} chooses {chosenGesture.name}");
+            }
+        }
+    }
+}
