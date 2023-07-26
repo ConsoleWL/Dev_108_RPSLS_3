432647d4c72118d7e10a829316b71661383da8a0
Nikita Obergan <Obergannikita@gmail.com>
2023-07-25 23:00:00 -0400

    Add abstract class Gesture

---
+++ RPSLS/Gesture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    public abstract class Gesture
+    {
+        public string name;
+
+        public abstract int WillBeat(Gesture gesture);
+    }
+}
